995b46bdcb3a049b0cae7c11520cdd1798c14cb3
Labing <Labing@XeonB101-PC-25.academia.loc>
2017-05-12 10:32:09 -0500

    Insertar obra

--- src/main/java/edu/co/sergio/mundo/dao/DepartamentoDAO.java
+++ src/main/java/edu/co/sergio/mundo/dao/DepartamentoDAO.java
@@ -11,6 +11,7 @@ import edu.co.sergio.mundo.vo.Departamento;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import edu.co.sergio.mundo.vo.Obra;
 
 /**
  * @author Isabel-Fabian
@@ -26,58 +27,54 @@ import java.util.logging.Logger;
  */
  
 
-public class DepartamentoDAO implements IBaseDatos<Departamento> {
+public class DepartamentoDAO {
 
 	/**
 	 * Funcion que permite obtener una lista de los departamentos existentes en la base de datos
 	 * @return List<Departamento> Retorna la lista de Departamentos existentes en la base de datos
 	 */
-	public List<Departamento> findAll() {
-		List<Departamento> departamentos= null;
-	    String query = "SELECT * FROM Depto";
-	    Connection connection = null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	    try {
-	    Statement st = connection.createStatement();
-	    ResultSet rs = st.executeQuery(query);
-	    int id =0;
-	    String nombre = null;
-	
-	    while (rs.next()){
-	    	if(departamentos == null){
-	    		departamentos= new ArrayList<Departamento>();
-	    	}
-	      
-	        Departamento registro= new Departamento();
-	        id = rs.getInt("id_depto");
-	        registro.setId_departamento(id);
-	        
-	        nombre = rs.getString("nom_depto");
-	        registro.setNom_departamento(nombre) ;
-	        
-	        departamentos.add(registro);
-	    }
-	    st.close();
-	    
-	    } catch (SQLException e) {
-			System.out.println("Problemas al obtener la lista de Departamentos");
-			e.printStackTrace();
-		}
-	    
-	    return departamentos;
-	}
+//	public List<Departamento> findAll() {
+//		List<Departamento> departamentos= null;
+//	    String query = "SELECT * FROM Depto";
+//	    Connection connection = null;
+//            try {
+//                connection = Conexion.getConnection();
+//            } catch (URISyntaxException ex) {
+//                Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//	    try {
+//	    Statement st = connection.createStatement();
+//	    ResultSet rs = st.executeQuery(query);
+//	    int id =0;
+//	    String nombre = null;
+//	
+//	    while (rs.next()){
+//	    	if(departamentos == null){
+//	    		departamentos= new ArrayList<Departamento>();
+//	    	}
+//	      
+//	        Departamento registro= new Departamento();
+//	        id = rs.getInt("id_depto");
+//	        registro.setId_departamento(id);
+//	        
+//	        nombre = rs.getString("nom_depto");
+//	        registro.setNom_departamento(nombre) ;
+//	        
+//	        departamentos.add(registro);
+//	    }
+//	    st.close();
+//	    
+//	    } catch (SQLException e) {
+//			System.out.println("Problemas al obtener la lista de Departamentos");
+//			e.printStackTrace();
+//		}
+//	    
+//	    return departamentos;
+//	}
 
 	
-	/**
-	 * Funcion que permite realizar la insercion de un nuevo registro en la tabla Departamento
-	 * @param Departamento recibe un objeto de tipo Departamento 
-	 * @return boolean retorna true si la operacion de insercion es exitosa.
-	 */
-	public boolean insert(Departamento t) {
+	
+	public boolean insert(Obra obra) {
 		boolean result=false;
 		Connection connection=null;
             try {
@@ -85,12 +82,15 @@ public class DepartamentoDAO implements IBaseDatos<Departamento> {
             } catch (URISyntaxException ex) {
                 Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
-	    String query = " insert into Depto (id_depto,nom_depto)"  + " values (?,?)";
+	    String query = " insert into obra_de_Arte (usuario, nombre,  descripcion, estilo, valor)"  + " values (?, ?, ?, ?, ?)";
         PreparedStatement preparedStmt=null;
 	    try {
 			preparedStmt = connection.prepareStatement(query);
-			preparedStmt.setInt (1, t.getId_departamento());
-                        preparedStmt.setString (2, t.getNom_departamento());
+			preparedStmt.setString (1, obra.getUsuario());
+                        preparedStmt.setString (2, obra.getNombreObra());
+                        preparedStmt.setString (3, obra.getDescp());
+                        preparedStmt.setString (4, obra.getEstilo());
+                        preparedStmt.setInt (2, obra.getValor);
 			result= preparedStmt.execute();
 	    } catch (SQLException e) {
 			e.printStackTrace();
@@ -103,54 +103,54 @@ public class DepartamentoDAO implements IBaseDatos<Departamento> {
 	 * @param Departamento recibe un objeto de tipo Departamento 
 	 * @return boolean retorna true si la operacion de actualizacion es exitosa.
 	 */
-	public boolean update(Departamento t) {
-		boolean result=false;
-		Connection connection= null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-		String query = "update Depto set nom_depto = ? where id_depto = ?";
-		PreparedStatement preparedStmt=null;
-		try {
-		    preparedStmt = connection.prepareStatement(query);
-		    preparedStmt.setString(1, t.getNom_departamento());
-                    preparedStmt.setInt   (2, t.getId_departamento());
-		    if (preparedStmt.executeUpdate() > 0){
-		    	result=true;
-		    }
-			    
-		} catch (SQLException e) {
-				e.printStackTrace();
-		}
-			
-		return result;
-	}
+//	public boolean update(Departamento t) {
+//		boolean result=false;
+//		Connection connection= null;
+//            try {
+//                connection = Conexion.getConnection();
+//            } catch (URISyntaxException ex) {
+//                Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//		String query = "update Depto set nom_depto = ? where id_depto = ?";
+//		PreparedStatement preparedStmt=null;
+//		try {
+//		    preparedStmt = connection.prepareStatement(query);
+//		    preparedStmt.setString(1, t.getNom_departamento());
+//                    preparedStmt.setInt   (2, t.getId_departamento());
+//		    if (preparedStmt.executeUpdate() > 0){
+//		    	result=true;
+//		    }
+//			    
+//		} catch (SQLException e) {
+//				e.printStackTrace();
+//		}
+//			
+//		return result;
+//	}
 
 	/**
 	 * Funcion que permite realizar la eliminario de registro en la tabla Departamento
 	 * @param Departamento recibe un objeto de tipo Departamento 
 	 * @return boolean retorna true si la operacion de borrado es exitosa.
 	 */
-	public boolean delete(Departamento t) {
-	   boolean result=false;
-	   Connection connection = null;
-            try {
-                connection = Conexion.getConnection();
-            } catch (URISyntaxException ex) {
-                Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
-            }
-	   String query = "delete from Depto where id_depto = ?";
-	   PreparedStatement preparedStmt=null;
-	   try {
-		     preparedStmt = connection.prepareStatement(query);
-		     preparedStmt.setInt(1, t.getId_departamento());
-		    result= preparedStmt.execute();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	   
-	   return result;
-	}
+//	public boolean delete(Departamento t) {
+//	   boolean result=false;
+//	   Connection connection = null;
+//            try {
+//                connection = Conexion.getConnection();
+//            } catch (URISyntaxException ex) {
+//                Logger.getLogger(DepartamentoDAO.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//	   String query = "delete from Depto where id_depto = ?";
+//	   PreparedStatement preparedStmt=null;
+//	   try {
+//		     preparedStmt = connection.prepareStatement(query);
+//		     preparedStmt.setInt(1, t.getId_departamento());
+//		    result= preparedStmt.execute();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	   
+//	   return result;
+//	}
 }
